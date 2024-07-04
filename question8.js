6626a87e6668f73e3b000f967600e2e1fbed0af3
Milan Gautam <milangautam071440304@gmail.com>
2024-07-04 16:39:56 +0545

    question 8 is added

---
+++ question8.js
@@ -0,0 +1,8 @@
+//how to sort a given array of strings?
+/* 
+to sort an array of strings we will have to use a comparison method inside the sort mehod
+*/
+
+const studentList = ["ram", "shyam", "gita", "hari", "david", "sandeep"];
+const sortStudentList = studentList.sort();
+console.log(sortStudentList);
