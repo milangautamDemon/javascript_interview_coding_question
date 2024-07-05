b3af2f62aaadf99969b4b5f31e7e635a83b9332d
Milan Gautam <milangautam071440304@gmail.com>
2024-07-05 20:36:35 +0545

    question 15 is added

---
+++ question15.js
@@ -0,0 +1,8 @@
+//how to make a sentence out of a given array?
+/*
+we need to use array join method to combine elements from a given array
+*/
+
+const words = ["welcome", "to", "nepal."];
+const finalOutput = words.join(" ");
+console.log(finalOutput);
